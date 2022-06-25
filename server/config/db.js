739c6ea6a2e2ffe0c675da17630861289a81a2f1
johan <johan.13fernando@gmail.com>
2022-06-25 13:57:47 -0400

    MidTermTest2022

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"LocalURI": "mongodb://127.0.0.1/books229",
+  //MongoDBAtlas Link
+  "RemoteURI": "mongodb+srv://johanlema2022:6qaI0fklmQ5JbtvA@cluster0.r42ot.mongodb.net/books229?retryWrites=true&w=majority",
+  "HostName": "LocalHost",
+  "Secret": "someSecret"
+};
